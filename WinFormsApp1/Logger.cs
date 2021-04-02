602e0b8e1fc9181e2bb294b53c91380d650d6a41
Undertaker73 <linkorvb1@rambler.ru>
2021-04-02 19:31:30 +0400

    Исправил ошибку в логгере

--- WinFormsApp1/Logger.cs
+++ WinFormsApp1/Logger.cs
@@ -9,7 +9,7 @@ namespace WinFormsApp1
     {
         public static void LogException (string msg)
         {
-            using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + "log.txt", true))
+            using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + "\\log.txt", true))
             {
                 sw.WriteLine(DateTime.Today.ToString("dd.MM.yyyy") + "_" + DateTime.Now.ToString("HH:mm:ss_") + msg);
             }
